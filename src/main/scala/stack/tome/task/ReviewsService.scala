2baeb3a51c9a1ff03d804ebccf0b3252e065467b
Roman <romankryk1803@gmail.com>
2023-02-11 17:46:51 +0200

    Change case classes to traits and add fake implementations

--- src/main/scala/stack/tome/task/ReviewsService.scala
+++ src/main/scala/stack/tome/task/ReviewsService.scala
@@ -1,6 +1,7 @@
 package stack.tome.task
 
 import cats.implicits._
+import io.circe._
 import io.circe.parser._
 import org.http4s._
 import org.http4s.implicits.http4sLiteralsSyntax
@@ -10,64 +11,79 @@ import zio.interop.catz._
 import scala.util.chaining.scalaUtilChainingOps
 
 // TODO: add config
-case class ReviewsService(client: HttpClient) {
-  private lazy val domainsRequest = Request[Task](
-    method = Method.GET,
-    uri = uri"https://www.trustpilot.com/categories/jewelry_store?sort=latest_review",
-    headers = Headers(),
-  )
-
-  private def recentReviewsRequest(reviewsId: String) = Request[Task](
-    method = Method.GET,
-    uri = (uri"https://www.trustpilot.com/api/categoriespages/" / reviewsId
-      / Uri.Path.Segment("reviews")).withQueryParam("locale", "en-US"),
-    headers = Headers(),
-  )
-
-  private def parseDomainsData =
-    client(
-      _.expect[String](domainsRequest)
-        .map(
-          "(?<=href=\"/review/)[^\"]+|(?<=latest-reviews-)[^-]+"
-            .r
-            .findAllIn(_)
-            .distinct
-            .sliding(2, 2)
-            .map(l => (l.head, l.last))
-            .filter(_._2.lengthCompare(24) == 0)
-        )
-    )
-
-  private def parseReviewsData(reviewsId: String) =
-    client(
-      _.expect[String](recentReviewsRequest(reviewsId))
-        .map(
-          _.pipe(parse)
-            .map(_ \\ "reviews")
-            .map(_.headOption.flatMap(_.asArray).toList.flatten)
-        )
-    )
-
-  def getReviewsData = for {
-    domains <- parseDomainsData
-    result <- domains
-      .take(3) // TODO: remove it or change to maxDomains
-      .map {
-        // TODO: send to kafka
-        case (domain, reviewsId) =>
-          parseReviewsData(reviewsId)
-            .map(domain -> _)
-      }
-      .toVector
-      .sequence
-      .map(_.tap(println))
-  } yield result
+trait ReviewsService {
+  def getReviewsData: ZIO[Any, Throwable, Vector[(String, Either[ParsingFailure, List[Json]])]]
 
 }
 
 object ReviewsService {
-  lazy val layer =
-    ZLayer.fromFunction(ReviewsService.apply _)
+  lazy val layer: ZLayer[HttpClient, Nothing, ReviewsService] =
+    ZLayer.fromFunction((client: HttpClient) =>
+      new ReviewsService {
+        private lazy val domainsRequest = Request[Task](
+          method = Method.GET,
+          uri = uri"https://www.trustpilot.com/categories/jewelry_store?sort=latest_review",
+          headers = Headers(),
+        )
+
+        private def recentReviewsRequest(reviewsId: String) = Request[Task](
+          method = Method.GET,
+          uri = (uri"https://www.trustpilot.com/api/categoriespages/" / reviewsId
+            / Uri.Path.Segment("reviews")).withQueryParam("locale", "en-US"),
+          headers = Headers(),
+        )
+
+        private def parseDomainsData =
+          client(
+            _.expect[String](domainsRequest)
+              .map(
+                "(?<=href=\"/review/)[^\"]+|(?<=latest-reviews-)[^-]+"
+                  .r
+                  .findAllIn(_)
+                  .distinct
+                  .sliding(2, 2)
+                  .map(l => (l.head, l.last))
+                  .filter(_._2.lengthCompare(24) == 0)
+              )
+          )
+
+        private def parseReviewsData(reviewsId: String) =
+          client(
+            _.expect[String](recentReviewsRequest(reviewsId))
+              .map(
+                _.pipe(parse)
+                  .map(_ \\ "reviews")
+                  .map(_.headOption.flatMap(_.asArray).toList.flatten)
+              )
+          )
+
+        override def getReviewsData: ZIO[Any, Throwable, Vector[(String, Either[ParsingFailure, List[Json]])]] =
+          for {
+            domains <- parseDomainsData
+            result <- domains
+              .take(3) // TODO: remove it or change to maxDomains
+              .map {
+                // TODO: send to kafka
+                case (domain, reviewsId) =>
+                  parseReviewsData(reviewsId)
+                    .map(domain -> _)
+              }
+              .toVector
+              .sequence
+              .map(_.tap(println))
+          } yield result
+
+      }
+    )
+
+  lazy val fakeLayer: ZLayer[HttpClient, Nothing, ReviewsService] =
+    ZLayer.fromFunction((_: HttpClient) =>
+      new ReviewsService {
+        override def getReviewsData: ZIO[Any, Throwable, Vector[(String, Either[ParsingFailure, List[Json]])]] =
+          ZIO.succeed(Vector())
+
+      }
+    )
 
   def getReviewsData = ZIO.serviceWithZIO[ReviewsService](_.getReviewsData)
 
--- src/main/scala/stack/tome/task/TrafficService.scala
+++ src/main/scala/stack/tome/task/TrafficService.scala
@@ -8,27 +8,51 @@ import zio.interop.catz._
 import scala.util.chaining.scalaUtilChainingOps
 
 // TODO: add config
-case class TrafficService(client: HttpClient) {
-  private def trafficRequest(domain: String) = Request[Task](
-    method = Method.GET,
-    uri = uri"https://web.vstat.info/" / domain,
-    headers = Headers(
-      "authority" -> "web.vstat.info",
-      "cookie" -> "", // TODO: get it from config
-    ),
-  )
-
-  def getDomainTraffic(domain: String) =
-    client(
-      _.expect[String](trafficRequest(domain))
-        .map("(?<=VISITS\" data-datum=\")\\d+".r.findFirstIn(_).flatMap(_.toIntOption).tap(r => println(s"$domain $r")))
-    )
+trait TrafficService {
+  def getDomainTraffic(domain: String): Task[Option[Int]]
 
 }
 
 object TrafficService {
-  lazy val layer =
-    ZLayer.fromFunction(TrafficService.apply _)
+  lazy val layer: ZLayer[HttpClient, Nothing, TrafficService] =
+    ZLayer.fromFunction((client: HttpClient) =>
+      new TrafficService {
+        private def trafficRequest(domain: String): Request[Task] =
+          Request[Task](
+            method = Method.GET,
+            uri = uri"https://web.vstat.info/" / domain,
+            headers = Headers(
+              "authority" -> "web.vstat.info",
+              "cookie" -> "", // TODO: get it from config
+            ),
+          )
+
+        override def getDomainTraffic(domain: String): Task[Option[Int]] =
+          client(
+            _.expect[String](trafficRequest(domain))
+              .map(
+                "(?<=VISITS\" data-datum=\")\\d+"
+                  .r
+                  .findFirstIn(_)
+                  .flatMap(_.toIntOption)
+                  .tap(r => println(s"$domain $r"))
+              )
+          )
+
+      }
+    )
+
+  lazy val fakeLayer: ZLayer[HttpClient, Nothing, TrafficService] =
+    ZLayer.fromZIO(
+      ZIO
+        .service[HttpClient]
+        .map(_ =>
+          new TrafficService {
+            override def getDomainTraffic(domain: String): Task[Option[RuntimeFlags]] = ZIO.succeed(Some(666))
+
+          }
+        )
+    )
 
   def getDomainTraffic(domain: String) =
     ZIO.serviceWithZIO[TrafficService](_.getDomainTraffic(domain))
